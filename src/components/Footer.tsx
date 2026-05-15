66c9cf6e26319049d4fe090d306b80cb9fe6fdd2
samWidlund <sam.wid415@gmail.com>
2026-05-15 11:37:24 +0200

    footer: add info about tech stack

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export function Footer() {
     return (
-        <footer className="py-4 mt-4 text-center text-sm text-white bg-[rgba(30,30,30,0.85)]">
-            © samuel widlund 2026
+        <footer className="py-4 mt-4 text-sm text-center text-white bg-[rgba(30,30,30,0.85)]">
+            © samuel widlund 2026 <br />
+            <br />
+            created using react, tailwind, & typescript
         </footer>
     );
 }
